7cf17344c27edae682898de45b7955f1be3b5bc4
tkaing <t.kaing13@gmail.com>
2021-08-16 00:29:56 +0200

    Update drony.js

--- routes/drony.js
+++ routes/drony.js
@@ -109,22 +109,23 @@ router.get(
     '/live',
     async (request, response) => {
 
-            const config = {
-                    rtmp: {
-                            port: 1935,
-                            chunk_size: 60000,
-                            gop_cache: true,
-                            ping: 30,
-                            ping_timeout: 60
-                    },
-                    http: {
-                            port: 8000,
-                            allow_origin: '*'
-                    },
-            };
+        const config = {
+            logType: 3,
+            rtmp: {
+                port: 1935,
+                chunk_size: 60000,
+                gop_cache: true,
+                ping: 30,
+                ping_timeout: 60
+            },
+            http: {
+                port: 8000,
+                allow_origin: '*'
+            },
+        };
 
-            var nms = new NodeMediaServer(config)
-            nms.run();
+        var nms = new NodeMediaServer(config);
+        nms.run();
 
             return controller.json(response, "");
     }
